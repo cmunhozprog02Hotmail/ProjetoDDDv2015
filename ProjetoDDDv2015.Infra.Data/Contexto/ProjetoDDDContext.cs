ca1948dd6967ebdc690eae80e76756313194ca2c
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-05-18 20:05:31 -0300

    Instalação do Migrations Infra.Data 39:00

--- ProjetoDDDv2015.Infra.Data/Contexto/ProjetoDDDContext.cs
+++ ProjetoDDDv2015.Infra.Data/Contexto/ProjetoDDDContext.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using ProjetoDDDv2015.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjetoDDDv2015.Infra.Data.Contexto
 {
-    public class ProjetoDDDContext
+    public class ProjetoDDDContext : DbContext
     {
+        public ProjetoDDDContext() : base("ProjetoModelDDD")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
     }
+    
 }
