e3e6ed5ecc09418e8ffe4fd22c62a9addfd5192d
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-05-18 23:54:16 -0300

    ClienteConfiguration 52:48

--- ProjetoDDDv2015.Infra.Data/Contexto/ProjetoDDDContext.cs
+++ ProjetoDDDv2015.Infra.Data/Contexto/ProjetoDDDContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,42 @@ namespace ProjetoDDDv2015.Infra.Data.Contexto
         }
 
         public DbSet<Cliente> Clientes { get; set; }
-    }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+            
+        }
+        public override int SaveChanges()
+        {
+            foreach(var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+                if(entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadasto").IsModified = false;
+                }
+                {
+
+                }
+            }
+            return base.SaveChanges();
+        }
+    } 
     
 }
---
+++ ProjetoDDDv2015.Infra.Data/EntityConfig/ClienteConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using ProjetoDDDv2015.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ProjetoDDDv2015.Infra.Data.EntityConfig
+{
+    public class ClienteConfiguration : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteConfiguration()
+        {
+            HasKey(c => c.ClienteId);
+
+            Property(c => c.PrimeiroNome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(c => c.Sobrenome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(c => c.Email)
+                .IsRequired();
+
+                
+        }
+    }
+}
