2123c556dceef23090240eba407146881d120007
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-05-19 02:34:31 -0300

    1:54:00

---
+++ ProjetoDDDv2015.MVC/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoDDDv2015.MVC.ViewModels
+{
+    public class ClienteViewModel
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Prencha o Campo Nome")]
+        [MaxLength(150, ErrorMessage = "Máximo {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Mínimo {0} caracteres")]
+        public string PrimeiroNome { get; set; }
+
+        [Required(ErrorMessage = "Prencha o Campo Sobrenome")]
+        [MaxLength(150, ErrorMessage = "Máximo {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Mínimo {0} caracteres")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Prencha o Campo E-mail")]
+        [MaxLength(150, ErrorMessage = "Máximo {0} caracteres")]
+        [EmailAddress(ErrorMessage ="Preencha com um e-mail vádido")]
+        [DisplayName ("E-Mail")]
+        public string Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        //public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
---
+++ ProjetoDDDv2015.MVC/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ProjetoDDDv2015.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoDDDv2015.MVC.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage ="Campo Nome obrigatprio")]
+        [MaxLength(250, ErrorMessage ="Máxino {0} catacteres")]
+        [MinLength(2, ErrorMessage ="Mínimo de {2} catacteres")]
+        public string Nome { get; set; }
+
+        [DataType(DataType.Currency)]
+        
+        public decimal Valor { get; set; }
+
+        public bool Disponivel { get; set; }
+
+        public int ClienteId { get; set; }
+
+    }
+}
