334af43d07d944f03ea1b5e5b2b537e84cbc3788
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-05-19 00:34:14 -0300

    Criação de intefaces IRepositoryBase e IClienteReposiktory 1:09

---
+++ ProjetoDDDv2015.Domain/Interfaces/IClienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProjetoDDDv2015.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDDDv2015.Domain.Interfaces
+{
+    interface IClienteRepository : IRepositoryBase<Cliente>
+    {
+    }
+}
---
+++ ProjetoDDDv2015.Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDDDv2015.Domain.Interfaces
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        //Adicionar
+        void Add(TEntity obj);
+
+        // Selecionar
+        TEntity GetById(int id);
+        
+        // Listar Todos
+        IEnumerable<TEntity> GetAll();
+
+        // Updata
+        void Update(TEntity obj);
+
+        // Delete
+        void Remove(TEntity obj);
+
+        void Dispose();
+    }
+} 
